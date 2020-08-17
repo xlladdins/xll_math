45f1af61846c183b52562c269b2352fd03135a49
keithalewis <kal@kalx.net>
2020-08-16 22:56:48 -0400

    sync

---
+++ lexer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# lexer.py - scan typical language lines
+'''
+"aY to yank and @a to execute
+:!python3 -m pytest %
+'''
+import re
+
+# calls lambda on tok = m.group()
+def lexer(line): # and keys array argument
+	lex = re.Scanner([
+		(r"[a-zA-Z_]\w*", lambda lex, tok: ("IDENTIFIER", tok)),
+		(r"0x[0-9]+", lambda lex, tok: ("HEXIDECIMAL", tok)),
+		(r"[+-]?[0-9]+", lambda lex, tok: ("INTEGER", tok)),
+		(r"[\(\)\.,:=;]", lambda lex, tok: ("PUNCTUATION", tok)),
+		(r"\s+", lambda lex, tok: None),
+	], flags=re.DOTALL)
+	
+	return lex.scan(line)
+
+tests = {
+	"abc": ([('IDENTIFIER', 'abc')], ''),
+	"double f(int x);": (),
+}
+def test_lexer():
+	for key in tests:
+		print(key)
+		print(tests[key])
+		print(lexer(key))
+		#assert lexer(key) == tests[key]
+
+test_lexer()
