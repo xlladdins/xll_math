4ed9f1536735091af367af2d537fc6e627250333
keithalewis <kal@kalx.net>
2020-08-20 02:06:06 -0400

    sync

--- xllfloat.py
+++ xllfloat.py
@@ -13,13 +13,16 @@ href_xpath = tbody_xpath + "/a"
 def float_table(page):
 	return page.xpath(href_xpath)
 
+def float_href(href):
+	return [a.attrib['href'] for a in href]
+
 if __name__ == '__main__':
 	req = requests.get(float_url)
 	assert (req.status_code == 200)
 	# if r.status == 200: ...
 	parser = etree.HTMLParser(recover=True)
 	page = etree.HTML(req.content, parser)
-	article = [a.attrib['href'] for a in float_table(page)]
+	article = float_href(float_table(page))
 	#print(article)
 	for a in article:
 		print(a)
